402a4238b142a1377acb05b581c74e522ab1c1ee
Zach Raper <raperzac05@gmail.com>
2026-04-25 15:11:54 -0500

    Added fade-in scroll animation

---
+++ script.js
@@ -0,0 +1,20 @@
+const fadeElements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting)
+        {
+            entry.target.classList.add("show");
+        }
+        else
+        {
+            entry.target.classList.remove("show");
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+fadeElements.forEach((element) => {
+    observer.observe(element);
+});
