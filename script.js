9895edb77290b4361cf9fa39d065eb09bcc66729
Zach Raper <raperzac05@gmail.com>
2026-05-22 18:03:59 -0500

    added lightmode toggle to all pages, as well as pulling from the browsers preference

--- script.js
+++ script.js
@@ -19,11 +19,27 @@ fadeElements.forEach((el) => {
 
 //light/dark mode toggle
 const themeToggle = document.getElementById("theme-toggle");
+
+const savedTheme = localStorage.getItem("theme");
+const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
+
+if(savedTheme === "light" || (!savedTheme && prefersLight)) {
+    document.body.classList.add("light-mode");
+    themeToggle.textContent = "Dark Mode";
+} 
+else {
+    themeToggle.textContent = "Light Mode";
+}
+
 themeToggle.addEventListener("click", () => {
     document.body.classList.toggle("light-mode");
     if (document.body.classList.contains("light-mode")) {
+        localStorage.setItem("theme", "light");
         themeToggle.textContent = "Dark Mode";
-    } else {
+    } 
+    else {
+        themeToggle.textContent = "Light Mode";
+        localStorage.setItem("theme", "dark");
         themeToggle.textContent = "Light Mode";
     }
 });
